0f2ca12975b082dfa2fa2e20ff44e466c8c9a450
monchewharry <caodingxian@gmail.com>
2015-10-02 13:04:08 -0500

    clean the code

--- obama_tweet_graph/Obamacare_Tweets/time_series.R
+++ obama_tweet_graph/Obamacare_Tweets/time_series.R
@@ -1,25 +1,25 @@
 load("/Users/CDX/Google\ Drive/twitter_politics_data/obamacare.RData")# The data list is saved
 # str(obamacare[[100]])
 # obamacare[[100]]$created_at  
+library(ggplot2);library(dplyr)
 
-library(ggplot2)
-
-#### build the volume time series####
-x1 <- strptime(obamacare[[1]]$created_at
-               ,format="%a %b %d %H:%M:%S %z %Y",tz = "UTC")#see ?strftime for %
-time_stamp<-sapply(obamacare,FUN = function(x) return(as.POSIXct(x$created_at,
-                                              format="%a %b %d %H:%M:%S %z %Y",
-                                              tz = "UTC")))
-
-time_stamp<-time_stamp+(x1-time_stamp[1])##origin "1970-01-01 GMT"
-time_stamp<-strftime(time_stamp, format="%D")#day/month/year
-time_index<-time_stamp#copy
-
-
-(volume<-data.frame(table(time_index)))
-volume$time_index<-as.character(volume$time_index)
+#### 1. build the volume time series####
+load("volume.RData") 
+# x1 <- strptime(obamacare[[1]]$created_at
+#                ,format="%a %b %d %H:%M:%S %z %Y",tz = "UTC")#see ?strftime for %
+# time_stamp<-sapply(obamacare,FUN = function(x) return(as.POSIXct(x$created_at,
+#                                               format="%a %b %d %H:%M:%S %z %Y",
+#                                               tz = "UTC")))
+# 
+# time_stamp<-time_stamp+(x1-time_stamp[1])##origin "1970-01-01 GMT"
+# time_stamp<-strftime(time_stamp, format="%D")#day/month/year
+# time_index<-time_stamp#copy
+# 
+# 
+# (volume<-data.frame(table(time_index)))
+# volume$time_index<-as.character(volume$time_index)
 #save(volume,file = "volume.RData")
-load("volume.RData")  
+
 tweet_volume<-as.xts(volume$Freq,as.Date(volume$time_index,format = "%m/%d/%y"))
 
 #### volume plot #####
@@ -50,67 +50,69 @@ library(quantmod)
 #             subset="2012-10-22::2012-11-08")
 # dev.off()
 
-#### top 5 hashtag each day ####  
-cut_point<-which(!duplicated(time_index))[-1]-1 
-cut_point<-c(0,cut_point,length(time_index))#the point where the date changes  
+#### 2. daily top5 hashtag ####
+A<-read.csv("ts_list.csv",header = T)
 
-
-
-get_hash<-function(x){
-  sapply(x$entities$hashtags,function(y) return(y$text))
-}
+# cut_point<-which(!duplicated(time_index))[-1]-1 
+# cut_point<-c(0,cut_point,length(time_index))#the point where the date changes  
+# 
+# 
+# 
+# get_hash<-function(x){
+#   sapply(x$entities$hashtags,function(y) return(y$text))
+# }
 
 #lapply(obamacare[1:5], FUN = get_hash)
 
-hash_tags<-list()
-for(i in seq(length(cut_point)-1)){#collect hashtags for each tweet on each day
-  hash_period<-lapply(obamacare[cut_point[i]+1:cut_point[i+1]]
-                      ,FUN = get_hash)
-  hash_tags[[i]]<-hash_period
-}
-#save(hash_tags,file = "hash_tags.RData")
+# hash_tags<-list()
+# for(i in seq(length(cut_point)-1)){#collect hashtags for each tweet on each day
+#   hash_period<-lapply(obamacare[cut_point[i]+1:cut_point[i+1]]
+#                       ,FUN = get_hash)
+#   hash_tags[[i]]<-hash_period
+# }
+# #save(hash_tags,file = "hash_tags.RData")
+# 
+# top5<-function(x){
+#   freq<-table(unlist(x))
+#   sorted<-names(freq)[order(freq,decreasing = T)]
+#   if(length(sorted>5)) return(sorted[1:5])
+#   else return(sorted[1])
+# }
+# 
+# tophash<-t(sapply(hash_tags,FUN = top5))
+# colnames(tophash)<-c("top5_1","top5_2","top5_3","top5_4","top5_5")
+# tophash<-as.data.frame(tophash,stringsAsFactors = F)
+# 
+# A<-cbind(volume,tophash)#to merge the time series
+# library(dplyr)
+# A<-rename(A,volume=Freq)
+# write.csv(A,file="ts_list.csv")
 
-top5<-function(x){
-  freq<-table(unlist(x))
-  sorted<-names(freq)[order(freq,decreasing = T)]
-  if(length(sorted>5)) return(sorted[1:5])
-  else return(sorted[1])
-}
+#### 3. top_5 hashtag's daily freq #####
+B<-read.csv("ts_list2.csv")
 
-tophash<-t(sapply(hash_tags,FUN = top5))
-colnames(tophash)<-c("top5_1","top5_2","top5_3","top5_4","top5_5")
-tophash<-as.data.frame(tophash,stringsAsFactors = F)
-
-A<-cbind(volume,tophash)#to merge the time series
-library(dplyr)
-A<-rename(A,volume=Freq)
-write.csv(A,file="ts_list.csv")
-
-#### top_5 hashtag freq #####  
-load("hash_tags.RData")
-freq<-table(unlist(hash_tags))
-
-top_5<-names(freq)[order(freq,decreasing = T)][1:6] # the top 5 # over the whole period,two of them are the same
-
-dailyfreq<-function(x){
-  freq5<-NULL
-  for(tag in top_5){
-    freq5<-c(freq5,sum(unlist(x)==tag))
-  }
-  freq5
-}
-top5_freq<-t(sapply(hash_tags,FUN = dailyfreq))
-
-colnames(top5_freq)<-top_5
-top5_freq<-as.data.frame(top5_freq,stringsAsFactors = F)
-head(top5_freq)
-
-library(dplyr)
-top5_freq<-select((mutate(top5_freq,Obamacare=Obamacare+ObamaCare)),-ObamaCare)# merge the same hashtags
-
-(B<-cbind(volume,top5_freq))#to load volume first!
-B<-rename(B,volume=Freq)
-write.csv(B,file="ts_list2.csv")
+# load("hash_tags.RData") 
+# freq<-table(unlist(hash_tags))
+# top_5<-names(freq)[order(freq,decreasing = T)][1:6] # the top 5 # over the whole period,two of them are the same
+# 
+# dailyfreq<-function(x){
+#   freq5<-NULL
+#   for(tag in top_5){
+#     freq5<-c(freq5,sum(unlist(x)==tag))
+#   }
+#   freq5
+# }
+# top5_freq<-t(sapply(hash_tags,FUN = dailyfreq))
+# 
+# colnames(top5_freq)<-top_5
+# top5_freq<-as.data.frame(top5_freq,stringsAsFactors = F)
+# head(top5_freq)
+# 
+# top5_freq<-select((mutate(top5_freq,Obamacare=Obamacare+ObamaCare)),-ObamaCare)# merge the same hashtags
+# 
+# (B<-cbind(volume,top5_freq))#to load volume first!
+# B<-rename(B,volume=Freq)
+# write.csv(B,file="ts_list2.csv")
 
 
 # overlaid picture
@@ -121,7 +123,7 @@ SCOTUS<-as.xts(B$SCOTUS,as.Date(B$time_index,format = "%m/%d/%y"))
 p2<-as.xts(B$p2,as.Date(B$time_index,format = "%m/%d/%y"))
 healthcare<-as.xts(B$healthcare,as.Date(B$time_index,format = "%m/%d/%y"))
 
-#### ggplot2 ##### 
+## ggplot2
 b<-B[c(-2)]
 b$time_index<-as.Date(b$time_index,format = "%m/%d/%y")
 b1<-melt(b,id.vars = c("time_index"))
@@ -133,7 +135,8 @@ ggplot(b1) + geom_line(aes(x=time_index, y=value, colour=variable)) +
   labs( title = "daily freq of top5#")
 dev.off()
 
-# respective picture  without unuseful period   
+# respective picture  without unuseful period  
+
 # ggplot(b, aes(time_index, Obamacare)) +xlab("period")+
 #   ylab("hashtags")+
 #   geom_line(lty = 1,colour = 1)#delete useless period
@@ -160,7 +163,4 @@ chartSeries(p2,subset =period3)
 chartSeries(healthcare,subset =period1)
 chartSeries(healthcare,subset =period2)
 chartSeries(healthcare,subset =period3)
-
-t1<-select(B,-volume)[]
-ggplot(t1)
 
