76553fa03d1630ab876f35281f9342f7514c7eb7
Your Name <you@example.com>
2015-10-14 15:09:12 -0500

    split the data

--- labeled_tweet/readdata.R
+++ labeled_tweet/readdata.R
@@ -17,3 +17,17 @@ obamacare_labeled<-obamacare
 #save(obamacare_labeled,file="obamacare_labeled.RData")
 load("obamacare_labeled.RData")# The result list is saved
 obamacare_labeled[[1]]$ideology#new label field
+
+#####split data by ideology #####  
+tweet_liberal<-lapply(obamacare_labeled, FUN= function(x) if(identical(x$ideology,"L")) return(x))
+null.loc<-sapply(tweet_liberal, FUN= function(x) if(is.null(x)) return(FALSE) else return(TRUE))
+tweet_liberal<-tweet_liberal[null.loc]
+#str(tweet_liberal[[1]])  
+
+tweet_conserv<-obamacare_labeled[null.loc==FALSE] #find the conservation
+#str(tweet_conserv[[1]])  
+
+length(obamacare_labeled)==length(tweet_conserv)+length(tweet_liberal)
+
+save(tweet_conserv,file = "tweet_conserv.RData")
+save(tweet_liberal ,file = "tweet_liberal.RData")
