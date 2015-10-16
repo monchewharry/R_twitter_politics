be9a7d33dd619cb7afb638192edde5289932d92e
monchewharry <caodingxian@gmail.com>
2015-10-16 11:25:51 -0500

    most mentioned stat

---
+++ labeled_tweet/L/elites.R
@@ -0,0 +1,10 @@
+str(tweet_liberal[[1012]])
+
+## definition
+
+## elites: media accounts, politicians, organizations, pundits, and celebrities  
+tweet_liberal[[1012]]$user$verified#verified
+tweet_liberal[[1012]]$user$followers_count# num of followers
+
+
+## activist:express their political identities on user profile  
